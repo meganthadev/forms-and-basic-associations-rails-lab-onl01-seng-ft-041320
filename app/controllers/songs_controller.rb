d19beec4787fa22f8d75ddcdde0ac7756da430d4
Megan Horsfield <meganthadev@gmail.com>
2020-07-02 05:52:09 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -47,11 +47,8 @@ class SongsController < ApplicationController
   private
 
   def song_params
-<<<<<<< HEAD
     params.require(:song).permit(:title, :artist_name, :genre, song_notes: [])
-=======
-    params.require(:song).permit(:title, :artist, :genre, song_notes: [])
->>>>>>> eccab5ab96aad8e2648c5a9a8cd84b1c9c3886d2
+  
   end
 end
 
